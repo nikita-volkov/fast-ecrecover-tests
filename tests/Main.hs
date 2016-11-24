b7779808ff4c04e9ee1d35db9667023b5ceea926
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-11-25 01:21:18 +0300

    Update the tests to find the culprit

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,6 +1,9 @@
 module Main where
 
 import Rebase.Prelude
+import Test.Tasty
+import Test.Tasty.Runners
+import Test.Tasty.HUnit
 import qualified Blockchain.Data.Address as C
 import qualified Blockchain.Data.Code as B
 import qualified Blockchain.Data.RLP as H
@@ -13,15 +16,23 @@ import qualified Network.Haskoin.Crypto as A
 import qualified Network.Haskoin.Internals as G
 
 
-main :: IO ()
 main =
+  defaultMain $
+  testGroup ""
+  [
+    testCase "getPubKeyFromSignature_fast == getPubKeyFromSignature" getPubKeyFromSignatureImplementationsTest
+    ,
+    testCase "getPubKeyFromSignature_fast == derivePubKey" fastGetPubKeyFromSignatureEqualsDerivePubKeyTest
+    ,
+    testCase "getPubKeyFromSignature == derivePubKey" slowGetPubKeyFromSignatureEqualsDerivePubKeyTest
+  ]
+
+getPubKeyFromSignatureImplementationsTest =
   forM_ [0..100] $ \n -> do
     generateTransaction n >>= onTransaction
   where
     onTransaction transaction =
-      if fastPubKey == slowPubKey
-        then return ()
-        else die ("fastPubKey /= slowPubKey:\n" <> show fastPubKey <> "\n" <> show slowPubKey)
+      assertEqual "Unequal" slowPubKey fastPubKey
       where
         extendedSignature =
           extendedSignatureFromTransaction transaction
@@ -31,6 +42,38 @@ main =
           E.getPubKeyFromSignature_fast extendedSignature hash
         slowPubKey =
           F.getPubKeyFromSignature extendedSignature hash
+
+fastGetPubKeyFromSignatureEqualsDerivePubKeyTest =
+  forM_ [0..100] $ \n -> do
+    generateTransaction n >>= onTransaction
+  where
+    onTransaction transaction =
+      assertEqual "Unequal" (Just derivedPubKey) fastPubKey
+      where
+        extendedSignature =
+          extendedSignatureFromTransaction transaction
+        hash =
+          hashFromTransaction transaction
+        fastPubKey =
+          E.getPubKeyFromSignature_fast extendedSignature hash
+        derivedPubKey =
+          A.derivePubKey prvKey
+
+slowGetPubKeyFromSignatureEqualsDerivePubKeyTest =
+  forM_ [0..100] $ \n -> do
+    generateTransaction n >>= onTransaction
+  where
+    onTransaction transaction =
+      assertEqual "Unequal" (Just derivedPubKey) slowPubKey
+      where
+        extendedSignature =
+          extendedSignatureFromTransaction transaction
+        hash =
+          hashFromTransaction transaction
+        slowPubKey =
+          F.getPubKeyFromSignature extendedSignature hash
+        derivedPubKey =
+          A.derivePubKey prvKey
 
 prvKey :: A.PrvKey
 prvKey =
